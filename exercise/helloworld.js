7c562c78a57ebd53b8b3fa6717456bb5d9619a7d
Agbortoko <arreyagbortoko@gmail.com>
2023-03-14 02:47:46 +0100

    Grouped exercises in one folder

--- exercise/helloworld.js
+++ exercise/helloworld.js
@@ -2,17 +2,13 @@
 
 
 // Print Hello World on Console
-
 console.log("Hello World");
-
 
 // Alert Hello World on browser
 alert("Hello World");
 
-
 // Print Hello World on browser
 document.write("Hello World");
-
 
 // Print Hello World in a html section with id "content"
 document.getElementById("#content").innerHTML = "Hello World";
