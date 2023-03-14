8567c86bec71b0b661cb49a662b1e5ccb5601b30
Agbortoko <arreyagbortoko@gmail.com>
2023-03-14 03:34:32 +0100

    Modified example 2

--- README.md
+++ README.md
@@ -40,6 +40,11 @@ const secondNumber = 45;
 
 console.log(firsNumber +  secondNumber);
 
+// Using template literals to log on console.
+
+const sum = firsNumber + secondNumber;
+console.log(`The sum of ${firsNumber} and ${secondNumber} is ${sum}`);
+
 
 // Add two numbers entered by the user
 const thirdNumber = parseInt(prompt("Enter the first number"));
--- examples/add2numbers.js
+++ examples/add2numbers.js
@@ -4,9 +4,15 @@ const secondNumber = 45;
 
 console.log(firsNumber +  secondNumber);
 
+// Using template literals to log on console.
+
+const sum = firsNumber + secondNumber;
+console.log(`The sum of ${firsNumber} and ${secondNumber} is ${sum}`);
+
 
 // Add two numbers entered by the user
 const thirdNumber = parseInt(prompt("Enter the first number"));
 const fourthNumber = parseInt(prompt("Enter the second Number"));
 
 console.log(thirdNumber + fourthNumber);
+
