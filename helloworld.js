b8646ce78b32a571bccf7447b269b5aece8d0f47
Agbortoko <arreyagbortoko@gmail.com>
2023-03-14 02:44:59 +0100

    Modified First Exercise

--- helloworld.js
+++ helloworld.js
@@ -1,8 +1,18 @@
+// Exercise 1
 
-/**
- * Exercise 1
- * Print Hello World on console
-*/
-function sayHello(){
-    console.log("Hello World");
-}
+
+// Print Hello World on Console
+
+console.log("Hello World");
+
+
+// Alert Hello World on browser
+alert("Hello World");
+
+
+// Print Hello World on browser
+document.write("Hello World");
+
+
+// Print Hello World in a html section with id "content"
+document.getElementById("#content").innerHTML = "Hello World";
